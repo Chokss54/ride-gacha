230c9d94d18ee6fc43290023094818d1aec6b610
Chokss54 <joelyang54@gmail.com>
2024-06-04 18:16:51 +1000

    Added DisplayContainer to view added drivers and passengers

---
+++ ride-gacha/src/components/DisplayContainer.tsx
@@ -0,0 +1,36 @@
+import Driver from "../model/Driver";
+import User from "../model/User";
+
+interface DisplayContainerProps {
+  titleContainer?: string;
+  userList: User[]
+}
+
+const DisplayContainer: React.FC<DisplayContainerProps> = ({ titleContainer, userList }) => {
+
+
+  return (
+    <div className="flex flex-auto flex-col mb-8">
+      <div className="relative w-64">
+        {titleContainer && (
+          <label className="font-poppins block text-gray-700 text-lg font-bold mb-2">
+            {titleContainer}
+          </label>
+        )}
+      </div>
+      <div className="w-auto h-48 overflow-y-auto bg-gray-100 p-4 border border-gray-300 rounded-lg lg:w-96">
+        {userList.length === 0 ? (
+          <p className="text-center text-gray-500 font-poppins">No items available</p>
+        ) : (
+          userList.map((item, index) => (
+            <div key={index} className="font-poppins p-2 border-b border-gray-300">
+              <p >{item.name}</p>
+            </div>
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default DisplayContainer;
